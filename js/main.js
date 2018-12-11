d418ee3d50ee32730bb97da8b9d7cfb676c5777c
mattegerton <matthewjegerton@googlemail.com>
2018-12-11 15:16:50 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$('#view-work').on('click', function () {
+    const images = $('#images').position().top;
+
+    $('html, body').animate({
+        scrollTop: images
+    }, 900)
+})
+
+$('#view-contact').on('click', function () {
+    const images = $('#contact').position().top;
+
+    $('html, body').animate({
+        scrollTop: images
+    }, 900)
+})
